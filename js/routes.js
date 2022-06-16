10617c47fe5c70960235671ee9dc3c73e527a0ad
Eros <eros.messy@gmail.com>
2022-06-16 19:23:46 -0400

    update

--- js/routes.js
+++ js/routes.js
@@ -7,16 +7,6 @@ var routes = [
   },
 
   {
-    path: '/about/',
-    url: './pages/about.html',
-  },
-
-  {
-    path: '/form/',
-    url: './pages/form.html',
-  },
-
-  {
     name: 'pregunta1', 
     path: '/pregunta1/',
     url: './pages/pregunta1.html',
@@ -31,6 +21,133 @@ var routes = [
     name: 'pregunta2',
     path: '/pregunta2/',
     url: './pages/pregunta2.html',
+    keepAlive: true,
+    options: {
+      transition: 'f7-cover',
+    },
+  },
+
+  {
+    name: 'page1',
+    path: '/page1/',
+    url: './pages/page1.html',
+    keepAlive: true,
+    options: {
+      transition: 'f7-cover',
+    },
+  },
+  {
+    name: 'page2',
+    path: '/page2/',
+    url: './pages/page2.html',
+    keepAlive: true,
+    options: {
+      transition: 'f7-cover',
+    },
+  },
+  {
+    name: 'page3',
+    path: '/page3/',
+    url: './pages/page3.html',
+    keepAlive: true,
+    options: {
+      transition: 'f7-cover',
+    },
+  },
+  {
+    name: 'page4',
+    path: '/page4/',
+    url: './pages/page4.html',
+    keepAlive: true,
+    options: {
+      transition: 'f7-cover',
+    },
+  },
+  {
+    name: 'page5',
+    path: '/page5/',
+    url: './pages/page5.html',
+    keepAlive: true,
+    options: {
+      transition: 'f7-cover',
+    },
+  },
+  {
+    name: 'page6',
+    path: '/page6/',
+    url: './pages/page6.html',
+    keepAlive: true,
+    options: {
+      transition: 'f7-cover',
+    },
+  },
+  {
+    name: 'page6',
+    path: '/page6/',
+    url: './pages/page6.html',
+    keepAlive: true,
+    options: {
+      transition: 'f7-cover',
+    },
+  },
+  {
+    name: 'page7',
+    path: '/page7/',
+    url: './pages/page7.html',
+    keepAlive: true,
+    options: {
+      transition: 'f7-cover',
+    },
+  },
+  {
+    name: 'page8',
+    path: '/page8/',
+    url: './pages/page8.html',
+    keepAlive: true,
+    options: {
+      transition: 'f7-cover',
+    },
+  },
+  {
+    name: 'page9',
+    path: '/page9/',
+    url: './pages/page9.html',
+    keepAlive: true,
+    options: {
+      transition: 'f7-cover',
+    },
+  },
+  {
+    name: 'page10',
+    path: '/page10/',
+    url: './pages/page10.html',
+    keepAlive: true,
+    options: {
+      transition: 'f7-cover',
+    },
+  },
+  {
+    name: 'page11',
+    path: '/page11/',
+    url: './pages/page11.html',
+    keepAlive: true,
+    options: {
+      transition: 'f7-cover',
+    },
+  },
+  {
+    name: 'page12',
+    path: '/page12/',
+    url: './pages/page12.html',
+    keepAlive: true,
+    options: {
+      transition: 'f7-cover',
+    },
+  },
+  {
+    name: 'page13',
+    path: '/page13/',
+    url: './pages/page13.html',
     keepAlive: true,
     options: {
       transition: 'f7-cover',
